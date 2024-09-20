76288eb365f45991f6da3dbaffdcd5ca87c32c76
Mirelle <mirellebeatriz7@gmail.com>
2024-09-20 10:49:22 -0300

    alterando em create

--- setup/urls.py
+++ setup/urls.py
@@ -6,7 +6,7 @@ from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDelete
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", TodoListView.as_view(), name="todo_list"),
-    path("create", TodoCreateView.as_view(), name="todo_create"),
+    path("create/", TodoCreateView.as_view(), name="todo_create"),
     path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
     path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
     path("complete/<int:pk>", TodoCompleteView.as_view(), name="todo_complete")
