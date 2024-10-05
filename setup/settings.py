8964d64f504ad1ac016eaac188bb7e9853a23b62
Mirelle <mirellebeatriz7@gmail.com>
2024-10-05 20:04:13 -0300

    Realizando correções

--- setup/settings.py
+++ setup/settings.py
@@ -30,9 +30,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool, default=True)
+DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="listadetarefasltd.com,www.listadetarefasltd.com")
+ALLOWED_HOSTS = ('127.0.0.1', 'localhost', 'listadetarefasltd.onrender.com')
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://listadetarefasltd.onrender.com"
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -64,7 +73,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'setup.middleware.RedirectToCustomDomainMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
