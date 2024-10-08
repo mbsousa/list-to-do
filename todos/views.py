5bb92924801fbb553ce0a520ce2360a6e5a630d1
Mirelle <mirellebeatriz7@gmail.com>
2024-10-07 22:40:15 -0300

    Corrigindo conflitos

--- todos/views.py
+++ todos/views.py
@@ -9,6 +9,11 @@ class TodoListView(ListView):
     model = Todo
     template_name = 'todos/todo_list.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['todo_list'] = enumerate(context['object_list'], start=1)
+        return context
+
 class TodoCreateView(CreateView):
     model = Todo
     fields = ["titulo","entrega_dt"]
@@ -26,5 +31,5 @@ class TodoDeleteView(DeleteView):
 class TodoCompleteView(View):
     def get(self, request, pk):
         todo = get_object_or_404(Todo, pk=pk)
-        todo.mark_has_complete()
+        todo.mark_has_complete()    
         return redirect("todo_list")
